701304c47869e0370ecfce71a0f6925497c64d04
Samarth175 <samarth1751997@gmail.com>
2017-04-05 18:05:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,23 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
 
-//Change the text of the main-text div
-var element=document.getElementById('main-text');
-element.innerHTML='New Value';
-
-//Move the image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight() {
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function() {
-    var interval=setInterval(moveRight,50);
+button.onclick=function(){
+  //Create a request object
+  var request=new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  request.onreadystatechange=function(){
+      if(request.readyState===XMLHttpRequest.DONE){
+          //Take some action
+          if(request.status===200){
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
+      }
+      //Not done yet
+  }
+  
+  //Make the request
+  request.open('GET','http://samarth175.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
