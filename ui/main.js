faeeb007fff40862a0fd8b058472a2b8df1fc399
Samarth175 <samarth1751997@gmail.com>
2017-05-03 00:00:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,5 +50,4 @@ submit.onclick = function() {
   var nameInput=document.getElementById('name');
   var name=nameInput.value;
   request.open('GET','http://samarth175.imad.hasura-app.io/submit-name?name=' +name,true);
-  request.send(null);
 };
