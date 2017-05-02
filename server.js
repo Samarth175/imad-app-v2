51f4899147a6f2828bb57a54e7e905692364553e
Samarth175 <samarth1751997@gmail.com>
2017-05-02 23:45:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,6 +89,16 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name',function (req, res) {    // /submit-name?name=xxx
+    //Get the name from the request
+    var name=req.query.name;    //express framework
+    
+    names.push(name);
+    //JSON Javascript Object Notation
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName',function (req, res) {
     //articleName==article-one
     //articles[articleName]=={} articles object for articles
